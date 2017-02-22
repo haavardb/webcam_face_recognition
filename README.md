50ab3f67b741b6bf80037629ae1d430b9216d6ee
Håvard <haavardb50@gmail.com>
2017-02-22 22:16:15 +0100

    Script now ads new faces to faces folder

--- README.md
+++ README.md
@@ -3,16 +3,9 @@
 This python project uses the [OpenCV](http://opencv.org/) library to recognize the person visible in webcam.
 
 ## Faces to recognize
-Before you can run the recognizer you have to give it some images to train with. Upload these to the faces folder with the pattern "subject{number}."
-Each subject you want to recognize should also have a corresponding .json file. subject{number}.json
-
-`{
-  "first_name":"foo",
-  "last_name":"bar"
-}`
+The script uses the webcam to find a face, and then if it doesn't find it in the existing images, it ads it to the folder. All the images are saved with a corresponding .json file which saves the name of the person.
 
 ## Install
 1. Download/clone this repo
 2. pip install -r ./requirements.txt
-4. Upload images to faces folder
 3. Run: python face_recognizer.py
--- face_recognizer.py
+++ face_recognizer.py
@@ -7,6 +7,8 @@ import logging as log
 import datetime as dt
 import json
 import progressbar
+import speech_recognition as sr
+import random
 
 from playsound import playsound
 from gtts import gTTS
@@ -28,10 +30,14 @@ path = './faces'
 
 # Minimum confident that the recognizer has to have to make a match
 # Closer to 0, the more confident
-# I found 45 to be a good number, but this may vary..
-minConf = 45
+# I found 40 to be a good number, but this may vary..
+minConf = 40
 
 secondsBetweenOutput = 10
+
+# Google cloud credentials
+with open('google_cloud.json', 'r') as outfile:
+    GOOGLE_CLOUD_SPEECH_CREDENTIALS = outfile.read()
 
 def get_images_and_labels(path):
 
@@ -46,7 +52,7 @@ def get_images_and_labels(path):
             # Read the image and convert to grayscale
             image_pil = Image.open(image_path).convert('L')
             image = np.array(image_pil, 'uint8')
-            nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
+            nbr = int(os.path.split(image_path)[1].split(".")[0])
             faces = faceCascade.detectMultiScale(image)
             count = count+1
             # If face is detected, append the face to images and the label to labels
@@ -59,65 +65,123 @@ def get_images_and_labels(path):
 def seconds_between(d1, d2):
     return abs((d2 - d1).seconds)
 
+def main():
 
-# Call the get_images_and_labels function and get the face images and the
-# corresponding labels
-images, labels = get_images_and_labels(path)
-cv2.destroyAllWindows()
+    fucking_mac_files_paths = [os.path.join(path, f) for f in os.listdir(path)  if f.endswith('.DS_Store')]
+    for fucking_mac_files_path in fucking_mac_files_paths:
+        os.remove(fucking_mac_files_path)
 
-# Perform the tranining
-recognizer.train(images, np.array(labels))
+    # Call the get_images_and_labels function and get the face images and the
+    # corresponding labels
+    images, labels = get_images_and_labels(path)
+    cv2.destroyAllWindows()
 
-while True:
-    if not video_capture.isOpened():
-        print('Unable to load camera.')
-        sleep(5)
-        pass
+    # Perform the tranining
+    recognizer.train(images, np.array(labels))
 
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+    while True:
+        if not video_capture.isOpened():
+            print('Unable to load camera.')
+            sleep(5)
+            pass
 
-    # convert image to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
 
-    faces = faceCascade.detectMultiScale(
-        gray
-    )
+        # convert image to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    for (x, y, w, h) in faces:
+        faces = faceCascade.detectMultiScale(
+            gray
+        )
 
-        # Draw a rectangle around the face
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
+        uniqueSubjects = set(labels)
 
-        cv_img = frame.astype(np.uint8)
-        cv_gray = cv2.cvtColor(cv_img, cv2.COLOR_RGB2GRAY)
+        for (x, y, w, h) in faces:
 
-        cv2.imshow("input", cv_gray)
+            # Draw a rectangle around the face
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 255), 3)
 
-        predict_image = np.array(cv_gray, 'uint8')
-        nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
+            cv_img = frame.astype(np.uint8)
+            cv_gray = cv2.cvtColor(cv_img, cv2.COLOR_RGB2GRAY)
 
-        if 'lastMatch' not in locals():
-            lastMatch = 0
-            matchTime = dt.datetime.utcnow()
+            cv2.imshow("input", cv_gray)
 
-        if conf < minConf:
-            secBtw = seconds_between(matchTime, dt.datetime.utcnow())
-            if (lastMatch != nbr_predicted) or (secBtw > secondsBetweenOutput):
-                with open(path+'/subject'+str(nbr_predicted)+'.json') as data_file:
-                    data = json.load(data_file)
-                    print "Predicted as: {}, conf: {}".format(data["first_name"].encode('utf-8'), conf)
-                    sayText = "Heisann " + data["first_name"].encode('utf-8')
+            predict_image = np.array(cv_gray, 'uint8')
+            # nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
+            nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
+
+            if 'lastMatch' not in locals():
+                lastMatch = 0
+                matchTime = dt.datetime.utcnow()
+
+            if 'hasMatch' not in locals():
+                hasMatch = False
+                faceFoundTime = dt.datetime.utcnow()
+
+            print "Predicted as: {}, conf: {}".format(nbr_predicted, conf)
+
+            if conf < minConf:
+                faceFoundTime = dt.datetime.utcnow()
+                secBtw = seconds_between(matchTime, dt.datetime.utcnow())
+                if (lastMatch != nbr_predicted) or (secBtw > secondsBetweenOutput):
+                    # print "Predicted as: {}".format(nbr_predicted)
+                    with open(path+'/'+str(nbr_predicted)+'.json') as data_file:
+                        data = json.load(data_file)
+                        print "Predicted as: {}, conf: {}".format(data["first_name"].encode('utf-8'), conf)
+                        sayText = "Hei " + data["first_name"].encode('utf-8') + "!"
+                        tts = gTTS(text=sayText, lang='no')
+                        tts.save("hello.mp3")
+                        playsound('hello.mp3')
+                        lastMatch = nbr_predicted
+                        matchTime = dt.datetime.utcnow()
+                        # subjectNo = len(uniqueSubjects)+1
+                        imgName = os.path.join(path, str(nbr_predicted)+'.'+str(random.randint(1, 100000))+'.png')
+                        # cv2.imwrite(imgName, predict_image)
+                        cv2.imwrite(imgName, cv_img[y: y + h, x: x + w])
+            else:
+                secBtwFaceFound = seconds_between(faceFoundTime, dt.datetime.utcnow())
+                print "No match found for {} seconds".format(secBtwFaceFound)
+                if secBtwFaceFound > 3:
+                    # sayText = "Sorry love, but i don't recognize your ugly fucking face."
+                    # tts = gTTS(text=sayText, lang='en-au')
+                    sayText = "Sorry ass, men jeg kjenner ikke igjen det stygge trynet ditt, men jeg skal huske deg til neste gang. Hva heter du?"
                     tts = gTTS(text=sayText, lang='no')
-                    tts.save("hello.mp3")
-                    playsound('hello.mp3')
-                    lastMatch = nbr_predicted
-                    matchTime = dt.datetime.utcnow()
-#        else:
-#            print "No match found..."
+                    tts.save("not_found.mp3")
+                    playsound('not_found.mp3')
+                    subjectNo = len(uniqueSubjects)+1
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)
+                    cv2.imwrite(os.path.join(path, '%d.'+str(random.randint(1, 100000))+'.png') % subjectNo, cv_img)
+                    # cv2.imwrite(os.path.join(path, '%d.'+str(random.randint(1, 100000))+'.png') % subjectNo, cv_img[y: y + h, x: x + w])
+
+                    # cv2.imwrite(imgName, )
+
+                    theName=raw_input('Input:')
+                    with open(os.path.join(path, '%d.json') % subjectNo, 'wb') as outfile:
+                        json.dump({'first_name': theName}, outfile)
+                        outfile.close()
+
+                    # r = sr.Recognizer()
+                    # with sr.Microphone() as source:
+                    #     audio = r.listen(source)
+                    #     theName = r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS, language="nb-NO")
+                    #     print("Google Cloud Speech thinks you said " + theName)
+                    #
+                    #     sayText = "Heisann " + theName.encode('utf-8')
+                    #     tts = gTTS(text=sayText, lang='no')
+                    #     tts.save("not_found.mp3")
+                    #     playsound('not_found.mp3')
+                    #     with open(os.path.join(path, '%d.json') % subjectNo, 'w') as outfile:
+                    #         json.dump({'first_name': theName}, outfile)
+
+                        main()
+                        break
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+main()
 
 # When everything is done, release the capture
 video_capture.release()
